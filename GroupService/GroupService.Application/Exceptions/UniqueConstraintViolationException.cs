04a2f8c21047d9a6dafd182bf92c0199c2c9108d
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-05-20 08:44:51 +0800

    feat: add UniqueConstraintViolationException

---
+++ GroupService/GroupService.Application/Exceptions/UniqueConstraintViolationException.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupService.Application.Exceptions;
+
+public class UniqueConstraintViolationException: Exception
+{
+    public UniqueConstraintViolationException(string inner, Exception ex) : base(inner, ex)
+    {
+        
+    }
+}
