c1ed7a74c12055f38a4ad20d9198a98e5fe6bfb1
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-05-23 23:33:50 +0800

    refactor: add startTime and endTime to EventProjection

--- GroupService/GroupService.Domain/Entities/EventProjection.cs
+++ GroupService/GroupService.Domain/Entities/EventProjection.cs
@@ -6,5 +6,7 @@ public class EventProjection
     public Guid EventId { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
     public List<GroupEvent> GroupEvents { get; set; } = [];
 }
