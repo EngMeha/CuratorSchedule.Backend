ff87b789326f0dad4c62651d6fc8d8f4f52594e5
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-08-09 22:37:21 +0800

    refactor: update group

--- GroupService/GroupService.Domain/Entities/Group.cs
+++ GroupService/GroupService.Domain/Entities/Group.cs
@@ -1,9 +1,30 @@
-﻿namespace GroupService.Domain.Entities;
+﻿using GroupService.Domain.Exceptions;
+
+namespace GroupService.Domain.Entities;
 
 public class Group
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int CountStudents { get; set; }
-    public List<GroupEvent> GroupEvents { get; set; } = [];
+    private readonly List<GroupEvent> _groupEvents = [];
+    
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int CountStudents { get; private set; }
+    public IReadOnlyCollection<GroupEvent> GroupEvents => _groupEvents.AsReadOnly();
+    
+    private Group() {}
+
+    public Group(string name, int countStudents, List<GroupEvent> groupEvents)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException("Group name cannot be null or empty.");
+        
+        if (countStudents <= 0)
+            throw new DomainException("Number of students must be greater than zero.");
+        
+        _groupEvents =  groupEvents is null ? new (): new(groupEvents);
+            
+        Id = Guid.NewGuid();
+        Name = name.Trim();
+        CountStudents = countStudents;
+    }
 }
