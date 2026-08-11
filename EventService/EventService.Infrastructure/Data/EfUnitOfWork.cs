3f7f0af145c09a3a5b4fdda684aaa83d3eae6b6c
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-08-11 20:09:21 +0800

    refactor: update UoW

--- EventService/EventService.Infrastructure/Data/EfUnitOfWork.cs
+++ EventService/EventService.Infrastructure/Data/EfUnitOfWork.cs
@@ -1,18 +1,29 @@
-﻿using EventService.Application.Interfaces;
+﻿using EventService.Application.Exceptions;
+using EventService.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace EventService.Infrastructure.Data;
 
 public class EfUnitOfWork: IUnitOfWork
 {
-    private readonly EventDbContext _eventDbContext;
+    private readonly EventDbContext _context;
 
-    public EfUnitOfWork(EventDbContext eventDbContext)
+    public EfUnitOfWork(EventDbContext context)
     {
-        _eventDbContext = eventDbContext;
+        _context = context;
     }
 
     public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        await _eventDbContext.SaveChangesAsync(cancellationToken);
+        try
+        {
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+        catch (DbUpdateException ex) when(ex.InnerException is PostgresException pgEx &&
+                                          pgEx.SqlState == PostgresErrorCodes.UniqueViolation)
+        {
+            throw new UniqueConstraintViolationException("Unique constraint violated.", ex);
+        }
     }
 }
